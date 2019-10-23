9e566ef6bceb850d0801d51562f2dc4f1886d332
CrogST <50702589+CrogST@users.noreply.github.com>
2019-10-23 09:18:03 +0300

    + фильтры

--- ip_class.h
+++ ip_class.h
@@ -3,10 +3,53 @@
 #include <string>
 #include <vector>
 
-//using vi = std::vector<int>;
-//template<typename T>
-//using v = std::vector<T>;
 using ip_t = std::vector<std::string>;
 using ip_list = std::vector<ip_t>;
 
-extern bool ip_cmp(ip_t a, ip_t b);
+class ip_class {
+
+    static auto _filter(ip_list list, ip_t::size_type) {
+        return list;
+    }
+
+    template <typename T, typename ...Args>
+    static auto _filter(ip_list list, ip_t::size_type cnt, T val, Args... args) {
+        decltype (list) ip1 = _filter(list, cnt+1, args...);
+        //или можно было бы без конечной функции:
+        //decltype (list) ip1;
+        //if(cnt == list.size() - 1) ip1 = list;
+        //else ip1 = _filter(list, cnt+1, args...);
+        decltype (list) ip2;
+        for(const auto & n : ip1) {
+            if(n[cnt].compare(val) == 0)
+                ip2.push_back(n);
+        }
+        return ip2;
+    }
+
+public:
+
+    static bool ip_cmp(ip_t a, ip_t b);
+
+    template <typename ...Args>
+    static auto filter(ip_list list, Args... args) {
+        return _filter(list, 0, std::to_string(args)...);
+    }
+
+    static auto filter_any(ip_list list, int val) {
+        decltype (list) list_out;
+        auto sval = std::to_string(val);
+        //пробегаемся по ip
+        for(const auto & ip : list) {
+            //пробегаемся по элементам текущего ip
+            for(const auto & n : ip) {
+                if(n.compare(sval) == 0) {
+                    list_out.push_back(ip);
+                    break;
+                }
+            }
+        }
+        return list_out;
+    }
+
+};
