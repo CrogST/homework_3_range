15652f724bfdb3f3a8dd69a9d79b4770de5ffdc5
CrogST <50702589+CrogST@users.noreply.github.com>
2019-10-23 09:17:42 +0300

    учитывается длина полей

--- ip_class.cpp
+++ ip_class.cpp
@@ -1,12 +1,19 @@
 #include "ip_class.h"
 
 //функция сравнения ip адресов
-//возвращает true, если первый адрес больше
-//иначе false.
-bool ip_cmp(ip a, ip b) {
-    for(unsigned int i = 0; i < a.size(); i++) {
-        if(a[i].compare(b[i]) > 0) return true;
-        else if(a[i].compare(b[i]) < 0) return false;
+//возвращает true, если первый адрес больше, иначе false.
+bool ip_class::ip_cmp(ip_t ip1, ip_t ip2) {
+    for(unsigned int i = 0; i < ip1.size(); i++) {
+        auto a = ip1[i];
+        auto b = ip2[i];
+        if(a.size() != b.size()) {
+            return a.size() > b.size();
+        } else {
+            if(a.compare(b) > 0)
+                return true;
+            else if(a.compare(b) < 0)
+                return false;
+        }
     }
     return false;
-};
+}
